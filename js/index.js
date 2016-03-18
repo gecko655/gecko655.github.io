30f415bf18b53efc99b61a01974af9044ae9a048
gecko655 <aqwsedrft1234@yahoo.co.jp>
2016-03-18 11:55:21 +0900

    Set on click listener to facebook link

--- js/index.js
+++ js/index.js
@@ -2,3 +2,6 @@ $("#line").click(function(){
   $("#fuckline").toggleClass("hide");
   console.log($("#fuckline"));
 });
+$("#facebook").click(function(){
+  $("#searchYourSelf").toggleClass("hide")
+})
